c5fc0f3b3ca995a7459bb799393e88ccb9d29d22
Joseph I Jess <jessj@linnbenton.edu>
2019-10-07 13:25:51 -0700

    Updated to include user input

--- inputs.c
+++ inputs.c
@@ -2,7 +2,15 @@
 
 int main()
 {
-	printf("Hello?");
+	int input_number;
+	
+	printf("input_number: %d\n", input_number);
+	
+	printf("Please enter a number: ");
+	
+	scanf("%d", &input_number);
+	
+	printf("\ninput_number: %d", input_number);
 
 	return 0;
 }
